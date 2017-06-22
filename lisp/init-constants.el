b30df05ea3c26d31154a2e55d281cf4c8d8c7ebe
振宗 <zhenzong.peng@icloud.com>
2017-06-22 18:38:46 +0800

    Change PYTHONPATH constant

--- lisp/init-constants.el
+++ lisp/init-constants.el
@@ -1,8 +1,9 @@
-(defconst PYTHON-PATH "/usr/local/bin/python"
-  "Define python path")
 (defconst BACKUP-DIR "~/.backups"
   "Define backup directory")
 (defconst LISP-PROGRAM-PATH "~/Workspace/Repository/svn/ccl/dx86cl64"
   "Define lisp program path")
+(defconst PYTHONPATH
+  '()
+  "Define python paths")
 
 (provide 'init-constants)
