f1b15a79ece8d84cda8bfefa4099c6a79d584535
振宗 <zhenzong.peng@icloud.com>
2017-06-30 17:36:53 +0800

    Update python template

--- templates/python-template.py
+++ templates/python-template.py
@@ -2,9 +2,11 @@
 # @author `(getenv (if (equal system-type 'windows-nt) "USERNAME" "USER")))`
 # @date `(format-time-string "%A, %e %B %Y.")`
 
+
 def ${1:`(let ((name (file-name-nondirectory (buffer-file-name))))(substring name 0 (- (length name) 3)))`}():
     $2
     pass
+
 
 if __name__ == '__main__':
     $1()
